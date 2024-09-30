a6f42b66ee3da35de308762c8f3f04bd6695238d
saberzero1 <github@emilebangma.com>
2024-09-30 11:40:17 +0200

    Fixes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,21 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [
+    Component.MobileOnly(
+      Component.ExplorerBurger({
+        folderClickBehavior: "link",
+        folderDefaultState: "collapsed",
+        useSavedState: true,
+        usePagePath: true,
+        title: "",
+      }),
+    ),
+    Component.MobileOnly(Component.PageTitle()),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+  ],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -17,7 +31,7 @@ export const sharedPageComponents: SharedLayout = {
 }
 
 // components for pages that display a single page (e.g. a single note)
-/*export const defaultContentPageLayout: PageLayout = {
+export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
@@ -41,17 +55,17 @@ export const sharedPageComponents: SharedLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.DesktopOnly(Component.Backlinks()),
   ],
-}*/
+}
 
 // components for pages that display lists of pages  (e.g. tags or folders)
-/*export const defaultListPageLayout: PageLayout = {
+export const defaultListPageLayout: PageLayout = {
   beforeBody: defaultContentPageLayout.beforeBody,
   left: defaultContentPageLayout.left,
   right: [],
-}*/
+}
 
 // components for pages that display a single page (e.g. a single note)
-export const defaultContentPageLayout: PageLayout = {
+/*export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
@@ -78,10 +92,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
-}
+}*/
 
 // components for pages that display lists of pages  (e.g. tags or folders)
-export const defaultListPageLayout: PageLayout = {
+/*export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
@@ -91,4 +105,4 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
-}
+}*/
