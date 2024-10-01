3c4bf0c2d3aacdf67c76cc640b7c20069626413a
saberzero1 <github@emilebangma.com>
2024-10-01 20:42:45 +0200

    Link themes

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,7 +15,8 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> with{" "}
+          <a href="https://github.com/saberzero1/quartz-themes">Quartz Themes</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
