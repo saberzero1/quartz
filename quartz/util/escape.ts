eb70df0946796cb42330b5f0126f98d0e7d23251
saberzero1 <github@emilebangma.com>
2024-12-15 14:40:08 +0100

    Excalidraw Quartz Syncer POC

--- quartz/util/escape.ts
+++ quartz/util/escape.ts
@@ -8,10 +8,23 @@ export const escapeHTML = (unsafe: string) => {
 }
 
 export const unescapeHTML = (unsafe: string) => {
+  const scripts = unsafe.match(/<script.*?>.*?<\/script>/g)
+  const getExcalidrawScripts = scripts?.filter((script) => script.includes("excalidraw"))
+  console.log(getExcalidrawScripts)
+  const escapedScripts = getExcalidrawScripts?.map((script) =>
+    script
+      .replaceAll("&amp;", "&")
+      .replaceAll("&lt;", "<")
+      .replaceAll("&gt;", ">")
+      .replaceAll("&quot;", '"')
+      .replaceAll("&#039;", "'"),
+  )
+  console.log(escapedScripts)
+  // replace excalidraw scripts with escaped versions
+  if (getExcalidrawScripts && escapedScripts) {
+    for (let i = 0; i < escapedScripts.length; i++) {
+      unsafe = unsafe.replace(getExcalidrawScripts[i], escapedScripts[i])
+    }
+  }
   return unsafe
-    .replaceAll("&amp;", "&")
-    .replaceAll("&lt;", "<")
-    .replaceAll("&gt;", ">")
-    .replaceAll("&quot;", '"')
-    .replaceAll("&#039;", "'")
 }
