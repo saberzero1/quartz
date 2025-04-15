97d198b6a8113a1447b358822831a30c4dada151
Emile Bangma <github@emilebangma.com>
2025-04-15 13:27:31 +0000

    Updated config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,33 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      folderDefaultState: "collapsed",
+      folderClickBehavior: "link",
+      useSavedState: true,
+      mapFn: (node) => {
+        return node
+      },
+      sortFn: (a, b) => {
+        // Sort order: folders first, then files. Sort folders and files alphabeticall
+        if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          return a.displayName.localeCompare(b.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+
+        if (!a.isFolder && b.isFolder) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+      filterFn: (node) => node.slugSegment !== "tags",
+      order: ["filter", "map", "sort"],
+    }),
   ],
   right: [
     Component.Graph(),
@@ -63,7 +89,33 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      folderDefaultState: "collapsed",
+      folderClickBehavior: "link",
+      useSavedState: true,
+      mapFn: (node) => {
+        return node
+      },
+      sortFn: (a, b) => {
+        // Sort order: folders first, then files. Sort folders and files alphabeticall
+        if ((!a.isFolder && !b.isFolder) || (a.isFolder && b.isFolder)) {
+          // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+          // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+          return a.displayName.localeCompare(b.displayName, undefined, {
+            numeric: true,
+            sensitivity: "base",
+          })
+        }
+
+        if (!a.isFolder && b.isFolder) {
+          return 1
+        } else {
+          return -1
+        }
+      },
+      filterFn: (node) => node.slugSegment !== "tags",
+      order: ["filter", "map", "sort"],
+    }),
   ],
   right: [],
 }
