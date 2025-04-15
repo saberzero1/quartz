1d78c5ca227236fcbb13c1c88d41c1f5d8fb906a
Emile Bangma <github@emilebangma.com>
2025-04-15 18:36:25 +0200

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.MobileOnly(Component.Spacer())],
+  header: [Component.MobileOnly(Component.PageTitle()), Component.MobileOnly(Component.Spacer())],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -28,7 +28,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.TagList(),
   ],
   left: [
-    Component.PageTitle(),
+    Component.DesktopOnly(Component.PageTitle()),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
@@ -52,7 +52,7 @@ export const defaultContentPageLayout: PageLayout = {
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
-    Component.PageTitle(),
+    Component.DesktopOnly(Component.PageTitle()),
     Component.MobileOnly(Component.Spacer()),
     Component.Flex({
       components: [
